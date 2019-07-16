93263ecd169e44573cf4f21420566cc41bb8dc1e
Etienne-Goujon <etienne.goujon@orange.fr>
2019-07-16 15:07:53 +0200

    UUID creation on install

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -17,6 +17,24 @@ chrome.runtime.onInstalled.addListener(function (details) {
 });
 
 
+function randomUUID() {
+    document.getElementById('uuid').value = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
+        chrome.storage.sync.set({uuid: uuid}, function() {
+     console.log('uuid saved ' + uuid);}
+        
+function copyText() {
+    document.getElementById('uuid').select()
+    document.execCommand('copy')
+}
+
+document.getElementById('generate').addEventListener('click', randomUUID)
+document.getElementById('copy').addEventListener('click', copyText)
+
+randomUUID()
+
+}
 
 function load_popup(data){
 
@@ -76,4 +94,3 @@ function load_popup(data){
 chrome.storage.sync.get(['email'], function(data) {
   load_popup(data);
 });
-
