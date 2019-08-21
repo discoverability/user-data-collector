afcc167de53702100b49f174ccec5f7a3dca590f
Etienne-Goujon <etienne.goujon@orange.fr>
2019-08-22 00:07:07 +0200

    Update popup.js

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var API_BASE="https://conso-api.vod-prime.space/"
-
 'use strict';
 
 /**
@@ -24,71 +22,27 @@ chrome.runtime.onInstalled.addListener(function (details) {
   }
 });
 
-function generatedUuid() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    )
-}
-
-function load_popup(data){
-
-  if('uuid' in data){
-
-    document.getElementById("configured").style.display="inline";
-    document.getElementById("not-configured").style.display="none";
-
-
-    document.getElementById("uuidCreated").innerHTML=data.uuid;
-
-    document.querySelector("#logslink a").onclick = function () {
-      chrome.tabs.create({active: true, url: API_BASE+data.uuid+"/logs"});
-    };
-
-    document.querySelector("#serverlinks a").onclick = function () {
-      chrome.tabs.create({active: true, url: API_BASE});
-    };
-
-    changeColor.onclick = function(element) {
-
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code:`
-            for(let a of document.querySelectorAll("#content #contents #contents a#thumbnail")){
-              const xhr = new XMLHttpRequest();
-              xhr.open('POST', '`+API_BASE+data.uuid+`/'+a.href.substring(32,43));
-              xhr.send();
-            }
-            `});
-      });
-    };
-    }
-    else{
-
-      document.getElementById('configured').style.display="none";
-      document.getElementById('not-configured').style.display="inline";
-      document.getElementById('uuid').value = generatedUuid();
-      document.getElementById('generateUuid').addEventListener('click', function() {
-        document.getElementById('waiting').style.display="block";
-        let uuid=document.getElementById('uuid').value;
-        chrome.storage.sync.set({uuid: uuid}, function() {
-          console.log('uuid saved ' + uuid);
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.storage.sync.get("uuid", function (obj) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code:`
+        for(let a of document.querySelectorAll("#content #contents #contents a#thumbnail")){
           const xhr = new XMLHttpRequest();
-          xhr.open('POST', API_BASE+uuid);
+          xhr.open('POST', 'https://streaming-sniffer-api.nextnet.top/`+obj.uuid+`/'+a.href.substring(32,43));
           xhr.send();
-          xhr.onreadystatechange=function(){
-            if (xhr.readyState==4 && xhr.status==200){
-              chrome.storage.sync.get(['uuid'], function(data2) {
-                load_popup(data2);
-              });
-            }
-          };
-        });
-        setTimeout(function() { document.location.reload(true) }, 5000);
-      });
-    }
+        }
+      `});
+    });
+});
 
-}
-chrome.storage.sync.get(['uuid'], function(data) {
-  load_popup(data);
+chrome.storage.sync.get("uuid", function (obj) {
+  document.getElementById('uuidCreated').innerText = obj.uuid;
+  document.querySelector("#logslink a").onclick = function () {
+    chrome.tabs.create({active: true, url: "https://streaming-sniffer-api.nextnet.top/"+obj.uuid+"/logs"});
+  };
+
+  document.querySelector("#serverlinks a").onclick = function () {
+    chrome.tabs.create({active: true, url: "https://streaming-sniffer-api.nextnet.top/"});
+  };
 });
