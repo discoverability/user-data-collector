b2d7ccc46f6e58d17d4e0ea76120f86c08f51179
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2020-04-29 00:26:34 +0200

    prepared automatic release 5.1.7

--- chrome_extension/release.sh
+++ chrome_extension/release.sh
@@ -4,16 +4,17 @@ export APP_VERSION="$3"
 export TARGET_SERVER="$4"
 export DEV_BUILD="$5"
 
-
+DIR="$(dirname "$0")"
 
 cp manifest.master $TARGET_FOLDER/manifest.json 
 
-
+#change metadata in manifest
 jq ".version = \"$APP_VERSION\"" $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
 jq ".name= \"$APP_NAME\"" $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
 jq 'del(.content_scripts)' $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
-jq '.content_scripts += [{ "matches": ["https://*.netflix.com/*"], "js": ["global.js","netflix_lib.js","netflix.js"]}]' $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
-jq ".content_scripts += [{\"matches\": [\"$TARGET_SERVER/set_robot\"],\"js\": [\"global.js\",\"emns.js\"]}]" $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
+jq ".permissions += [\"$TARGET_SERVER/\"]" $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
+jq '.content_scripts += [{ "matches": ["https://*.netflix.com/*"], "js": ["server.js","global.js","netflix_lib.js","netflix.js"]}]' $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
+jq ".content_scripts += [{\"matches\": [\"$TARGET_SERVER/set_robot\"],\"js\": [\"server.js\",\"global.js\",\"emns.js\"]}]" $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
 
 
 if [ $DEV_BUILD -eq 1 ]; then
@@ -24,5 +25,9 @@ else
 export RELEASE_TYPE="prod"
 fi
 
+#change production server URL
+echo "var BASE_URL=\"https://conso-api.vod-prime.space\";" > $TARGET_FOLDER/server.js
 
-cd $TARGET_FOLDER && zip -r ../$APP_NAME-$APP_VERSION-$RELEASE_TYPE.zip *
+cd $TARGET_FOLDER && zip -q -r ../$APP_NAME-$APP_VERSION-$RELEASE_TYPE.zip *
+cd .. && google-chrome-stable --pack-extension=$(basename $TARGET_FOLDER)|true
+echo $DIR/$TARGET_FOLDER/../$APP_NAME-$APP_VERSION-$RELEASE_TYPE.zip
