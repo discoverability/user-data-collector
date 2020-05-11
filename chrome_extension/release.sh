05fbc240caad15d625a18956ff1b03e24ec7b248
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2020-05-11 20:22:05 +0200

    prepared automatic release 5.1.17

--- chrome_extension/release.sh
+++ chrome_extension/release.sh
@@ -1,4 +1,6 @@
-export TARGET_FOLDER="$1"
+set -x 
+
+export TARGET_FOLDER="$(printf %q "$1")"
 export APP_NAME="$2"
 export APP_VERSION="$3"
 export TARGET_SERVER="$4"
@@ -6,20 +8,21 @@ export DEV_BUILD="$5"
 
 DIR="$(dirname "$0")"
 
-cp manifest.master $TARGET_FOLDER/manifest.json 
+export TARGET_FILE="$TARGET_FOLDER/manifest.json"
+cp manifest.master $TARGET_FILE 
 
 #change metadata in manifest
-jq ".version = \"$APP_VERSION\"" $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
-jq ".name= \"$APP_NAME\"" $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
-jq 'del(.content_scripts)' $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
-jq ".permissions += [\"$TARGET_SERVER/\"]" $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
-jq '.content_scripts += [{ "matches": ["https://*.netflix.com/*"], "js": ["server.js","global.js","netflix_lib.js","netflix.js"]}]' $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
-jq ".content_scripts += [{\"matches\": [\"$TARGET_SERVER/set_robot\"],\"js\": [\"server.js\",\"global.js\",\"emns.js\"]}]" $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
+jq ".version = \"$APP_VERSION\"" $TARGET_FILE  |sponge $TARGET_FILE 
+jq ".name= \"$APP_NAME\"" $TARGET_FILE  |sponge $TARGET_FILE 
+jq 'del(.content_scripts)' $TARGET_FILE  |sponge $TARGET_FILE 
+jq ".permissions += [\"$TARGET_SERVER/\"]" $TARGET_FILE  |sponge $TARGET_FILE 
+jq '.content_scripts += [{ "matches": ["https://*.netflix.com/*"], "js": ["server.js","global.js","netflix_lib.js","netflix.js"]}]' $TARGET_FILE  |sponge $TARGET_FILE 
+jq ".content_scripts += [{\"matches\": [\"$TARGET_SERVER/set_robot\"],\"js\": [\"server.js\",\"global.js\",\"emns.js\"]}]" $TARGET_FILE  |sponge $TARGET_FILE 
 
 
 if [ $DEV_BUILD -eq 1 ]; then
-jq '.permissions += ["http://localhost:5000/set_robot"]' $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
-jq '.content_scripts[1].matches += ["http://localhost:5000/set_robot"]' $TARGET_FOLDER/manifest.json  |sponge $TARGET_FOLDER/manifest.json 
+jq '.permissions += ["http://localhost:5000/set_robot"]' $TARGET_FILE  |sponge $TARGET_FILE 
+jq '.content_scripts[1].matches += ["http://localhost:5000/set_robot"]' $TARGET_FILE  |sponge $TARGET_FILE 
 export RELEASE_TYPE="dev"
 else
 export RELEASE_TYPE="prod"
