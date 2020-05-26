c4d5211ac2c588068162541ceadeaad767704146
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2020-05-27 00:54:41 +0200

    openapi api implementation

--- api/app/routes.py
+++ api/app/routes.py
@@ -2,10 +2,55 @@ from app.models import StreamLog, User, NetflixSuggestMetadata, NetflixWatchMeta
 from flask import make_response, render_template, abort, g, request, redirect, url_for
 from app import app
 from app import db
+import json
 
 import sqlalchemy
 
 from functools import wraps
+
+
+class SetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+@app.route("/dataviz-api/v1/users", methods=['GET'])
+def get_dataviz_users():
+    users = db.session.query(User).all()
+    data = {u.extension_id: {l.single_page_session_id for l in u.lolomos} for u in users}
+
+    return json.dumps([{"user_id": k, "session_ids": v} for k, v in data.items()], cls=SetEncoder)
+
+
+@app.route("/dataviz-api/v1/thumbnails/<user_id>/<session_id>", methods=['GET'])
+def get_thumbnails_data(user_id, session_id):
+    data=[]
+
+    suggests = (db.session.query(User, NetflixSuggestMetadata).order_by(NetflixSuggestMetadata.timestamp)
+                .filter(User.id == NetflixSuggestMetadata.user_id)
+                .filter(User.extension_id == user_id)
+                .order_by(NetflixSuggestMetadata.timestamp, NetflixSuggestMetadata.row, NetflixSuggestMetadata.rank)
+                .all())
+
+    suggests = {"%s/%03d/%03d" % (s.timestamp.strftime("%m%d%H%M"), s.row, s.rank): s for _, s in suggests}
+
+    # listings = [list(g) for  g in groupby(suggests, attrgetter('timestamp','row','rank'))]
+
+    res = "<html><body>#timestamp;ip;content_id;location;row;rank;app_view<br>"
+    for k_suggest in sorted(suggests):
+        suggest = suggests[k_suggest]
+        item={}
+        item["content_id"] = suggest .track_id
+        item["row"] = suggest .row
+        item["col"] = suggest .rank
+        data.append(item)
+    return json.dumps(data)
+
+
+
+
 
 
 def login_required(f):
@@ -27,7 +72,7 @@ def guard_ip(ip):
 def guard_user_consent(user):
     consent = db.session.query(UserMetaData).filter(UserMetaData.user_id == user.id).filter(
         UserMetaData.key == "consent-logs").first()
-    if consent is not None and consent.value=="false":
+    if consent is not None and consent.value == "false":
         abort(200)
 
 
